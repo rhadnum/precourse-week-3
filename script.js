305c3218c2d66a8ea79c6dbc84fb84bc403b0606
Rohan Hadnum <rhadnum@gmail.com>
2016-09-16 13:59:41 +0100

    added jquery

---
+++ script.js
@@ -0,0 +1,20 @@
+
+$(document).ready(function(){
+
+$('#button1').click(function (){
+
+	var url = "https://www.youtube.com/embed/aT8d27Jfdhs";
+
+	$('#iframe').attr('src', url);
+
+});
+
+$('#button2').click(function (){
+
+	var url = "https://www.youtube.com/embed/iA7jdnciv3o";
+
+	$('#iframe').attr('src', url);
+
+});
+
+});
